5d1f85dd0e70aa7dc880f2b59a82aef2221f3ad6
Allen Sarmiento <42989060+Allen-Sarmiento@users.noreply.github.com>
2018-09-16 16:46:20 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+int main() {
+	#include <iostream>
+	using namespace std;
+
+	cout << "Enter two numbers: ";
+	cin >> num1 >> num2;
+
+	cout << "The sum is ";
+	cout << sum;
+	cout << '\n';
+
+	int sum;
+
+	return 0;
+
+	int num1;
+	int num2;
+}
