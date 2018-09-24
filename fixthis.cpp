b9706d692ed08e38b755b9b859a07d4710051c17
Allen Sarmiento <42989060+Allen-Sarmiento@users.noreply.github.com>
2018-09-23 23:08:35 -0700

    Add files via upload

---
+++ fixthis.cpp
@@ -0,0 +1,11 @@
+
+int main() 
+{
+	#include <iostream>
+	cout << "You have " << homework << " assignments left.\n";
+	homework = 5;
+	homework = 12;
+	cout << "I have " << homework << " assignments left.\n";
+	int homework;
+}
+return 0;
